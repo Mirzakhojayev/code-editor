abe525c7879cdcce9978d9f990d1168108419d5e
Abdulloh <mirzakhojayevabdulloh@gmail.com>
2025-06-15 13:11:37 +0500

    Initial commit

---
+++ convex/schema.ts
@@ -0,0 +1,38 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+
+export default defineSchema({
+    users: defineTable({
+        userId: v.string(),
+        email: v.string(),
+        name: v.string(),
+    }).index("by_user_id", ["userId"]),
+
+    codeExecutions: defineTable({
+        userId: v.string(),
+        language: v.string(),
+        code: v.string(),
+        output: v.optional(v.string()),
+        error: v.optional(v.string()),
+    }).index("by_user_id", ["userId"]),
+
+    snippets: defineTable({
+        userId: v.string(),
+        title: v.string(),
+        language: v.string(),
+        code: v.string(),
+        userName: v.string(),
+    }).index("by_user_id", ["userId"]),
+
+    snippetCommets: defineTable({
+        snippetId: v.id("snippets"),
+        userId: v.string(),
+        userName: v.string(),
+        content: v.string(),
+    }).index("by_snippet_id", ["snippetId"]),
+
+    stars: defineTable({
+        
+    })
+})
